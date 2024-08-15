10d0aab6b86aa3fbd04f69c3a1301abc9fc4de56
deephouse025 <deephouse025@gmail.com>
2024-08-15 15:27:19 -0400

    moved project to vite

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import DailyActions from "../components/DailyActions";
+import EnterNewMax from "../components/EnterNewMax";
+import TodaysLift from "../components/TodaysLift";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+const Dashboard = () => {
+  return (
+    <div className="DashboardPage">
+      <Navbar/>
+        <DailyActions />
+        <EnterNewMax />
+        <TodaysLift />
+      <Footer/>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/Results.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Results = () => {
+  return (
+    <div className="ResultsPage">
+      <Navbar/>
+      <h1>Results page</h1>
+      <Footer/>
+    </div>
+  );
+};
+export default Results;
