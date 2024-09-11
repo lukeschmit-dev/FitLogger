3d9863244cdee8ae686120721ed7dbb4d790d68b
DXM <kimisaac010@gmail.com>
2024-09-11 19:44:11 +0900

    Daily tasks Set up.

--- src/components/DailyActionsForm.jsx
+++ src/components/DailyActionsForm.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 
-export const DailyActionsForm = () => {
+export const DailyActionsForm = ({setActions}) => {
   const [value, setValue] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(value);
+    setActions((prev)=>[
+      ...prev,
+      {title: value, completed: false}
+    ])
   };
+
   return (
     <form className="DailyActionsForm" onSubmit={handleSubmit}>
       <input
         type="text"
         className="DailyActions-input"
-        placeholder="New Task: "
+        placeholder="New Task"
         onChange={(e) => setValue(e.target.value)}
       />
       <button type="submit" className="DailyActionsBtn">
--- src/components/DailyActionsWrapper.jsx
+++ src/components/DailyActionsWrapper.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { DailyActionsForm } from "./DailyActionsForm";
 
+//🚧What is this for ?
 export const DailyActionsWrapper = () => {
   return (
     <div className="DailyActionsWrapper">
